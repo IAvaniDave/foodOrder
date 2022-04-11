5c8519bf7c9ac3bc21891e1058a70752576a0f4e
IAvaniDave <avani.dave@innvonix.com>
2022-04-11 23:39:39 +0530

    new db added

--- sql/foodorder.sql
+++ sql/foodorder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2022 at 09:13 PM
+-- Generation Time: Apr 11, 2022 at 08:09 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -59,12 +59,31 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`id`, `name`, `price`, `image`, `quantity`, `type`, `popular`, `delivery_time`, `restaurant_id`) VALUES
-(1, 'Rajwadi Undhiyu res 1', 50, NULL, 500, 1, 1, 20, 1),
-(2, 'Rajwadi Undhiyu res 1.1', 50, NULL, 500, 1, 1, 20, 1),
-(3, 'Rajwadi Undhiyu res 2', 50, NULL, 500, 1, 1, 20, 2),
-(4, 'Rajwadi Undhiyu res 3', 100, NULL, 500, 1, 1, 20, 3),
-(5, 'Rajwadi Undhiyu res 3.1', 150, NULL, 500, 1, 1, 20, 3),
-(6, 'Rajwadi Undhiyu res 2.1', 50, NULL, 500, 1, 1, 20, 2);
+(1, 'Rajwadi Undhiyu', 50, NULL, 500, 1, 1, 20, 1),
+(2, 'Paneer Handi', 120, NULL, 500, 1, 1, 20, 1),
+(3, 'Aloo matar', 50, NULL, 0, 1, 1, 20, 2),
+(4, 'Bhajipav', 100, NULL, 500, 1, 1, 20, 3),
+(5, 'Jain Bhaji with pav', 150, NULL, 500, 1, 1, 20, 3),
+(6, 'Cheese chilli', 100, NULL, 500, 1, 1, 20, 2),
+(7, 'Panjabi Thali', 170, NULL, 500, 1, 1, 20, 1),
+(8, 'Vegetable cheese sandwich', 80, NULL, 0, 1, 1, 20, 2),
+(9, 'Mexican sandwich', 120, NULL, 0, 1, 1, 20, 2),
+(10, 'Vegetable pulav', 150, NULL, 500, 1, 1, 20, 3),
+(11, 'Mahalaxmi special Bhaji', 130, NULL, 500, 1, 1, 20, 3),
+(12, 'Dal Bati', 80, NULL, 500, 1, 1, 20, 4),
+(13, 'Dal Bati with churma with chhas', 125, NULL, 500, 1, 1, 20, 4),
+(14, 'Dal tadka with bati', 130, NULL, 500, 1, 1, 20, 4),
+(15, 'Howzzat Biryani Combo', 130, NULL, 500, 1, 1, 20, 5),
+(16, 'Super 4s Bucket Meal', 190, NULL, 500, 1, 1, 20, 5),
+(17, '10 strips & 2 Dips Bucket', 199, NULL, 500, 1, 1, 20, 5),
+(18, 'Regular chicken shawarama', 199, NULL, 500, 1, 1, 20, 6),
+(19, 'Cheese chicken shawarama', 210, NULL, 500, 1, 1, 20, 6),
+(20, 'Paneer Masala Toast Sandwich', 120, NULL, 500, 1, 1, 20, 7),
+(21, 'Over The Top Cheese Burger', 110, NULL, 500, 1, 1, 20, 7),
+(22, 'Cheese Loaded Fries', 80, NULL, 500, 1, 1, 20, 7),
+(23, 'NEWtella Cheesecake Jar', 200, NULL, 500, 1, 1, 20, 8),
+(24, 'Belgian Triple Chocolate Mousse Cake Jar', 230, NULL, 500, 1, 1, 20, 8),
+(25, 'Dark Chocolate Walnut Brownie', 280, NULL, 500, 1, 1, 20, 8);
 
 -- --------------------------------------------------------
 
@@ -116,7 +135,7 @@ INSERT INTO `restaurants` (`id`, `name`, `city`, `rating`, `rating_count`, `star
 (4, 'Jain Dal Bati', 'Ahmedabad', 4, NULL, '10:00:00.000000', '20:00:00.000000', 5, 10, 20, 210, 25, 'item-4.webp'),
 (5, 'KFC', 'Vadodara', 4, NULL, '10:00:00.000000', '20:00:00.000000', 3, 10, 25, 220, 30, 'item-5.webp'),
 (6, 'Fire on Wheels', 'Vadodara', 4, NULL, '10:00:00.000000', '20:00:00.000000', 3, 10, 15, 230, 20, 'item-6.webp'),
-(7, 'SUgarless Life', 'Surat', 4, NULL, '10:00:00.000000', '20:00:00.000000', 3, 10, 15, 180, 25, 'item-7.webp'),
+(7, 'I love sandwiches', 'Surat', 4, NULL, '10:00:00.000000', '20:00:00.000000', 3, 10, 15, 180, 25, 'item-7.webp'),
 (8, 'SUgarless Life', 'Surat', 4, NULL, '10:00:00.000000', '20:00:00.000000', 3, 10, 25, 160, 30, 'item-8.webp');
 
 -- --------------------------------------------------------
@@ -193,7 +212,7 @@ ALTER TABLE `carts`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `orders`
